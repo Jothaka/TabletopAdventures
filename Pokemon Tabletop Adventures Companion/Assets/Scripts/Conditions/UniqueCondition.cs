d6986b39adfa0b4a20029a93f6e8245c657f445a
Nimeya <lord3nemesis@web.de>
2019-06-09 19:07:11 +0200

    no message

---
+++ Pokemon Tabletop Adventures Companion/Assets/Scripts/Conditions/UniqueCondition.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+//This is a description Condition only, as some terms can only be checked by the players themselves
+public class UniqueCondition : Condition
+{
+    public string ConditionDescription;
+}
---
+++ Pokemon Tabletop Adventures Companion/Assets/Scripts/FeatureButton.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FeatureButton : MonoBehaviour
+{
+    public event Action<string> OnFeatureSelected;
+
+    public Feature AssignedFeature;
+
+    [Header("Local References")]
+    public Text FeatureName;
+    public Text FeatureClassName;
+
+    private void Start()
+    {
+        FeatureClassName.text = AssignedFeature.FeatureClass.ToString();
+        var features = FeatureCollection.GetClassFeatures(AssignedFeature.FeatureClass);
+        FeatureName.text = features[AssignedFeature.FeatureID];
+    }
+
+    public void InvokeFeatureSelected()
+    {
+        OnFeatureSelected?.Invoke(AssignedFeature.FeatureEffect);
+    }
+}
